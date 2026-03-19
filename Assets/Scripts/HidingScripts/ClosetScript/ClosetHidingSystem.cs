eefcf8b2697c9a6c3bbce5256edbd09e2f8b9c39
jnacu <jmnacubuan1016@gmail.com>
2026-03-19 19:07:56 +0800

    Update on Closet Mechanic

--- Assets/Scripts/HidingScripts/ClosetScript/ClosetHidingSystem.cs
+++ Assets/Scripts/HidingScripts/ClosetScript/ClosetHidingSystem.cs
@@ -5,6 +5,10 @@ public class ClosetHidingSystem : MonoBehaviour
     public GameObject VcamPlayer;
     public GameObject VcamCloset;
 
+    public Transform player;
+    public Transform exitPoint;
+    public Rigidbody rb;
+
     public bool InsideCloset = false;
 
     public void GoInsideCloset()
@@ -16,7 +20,18 @@ public class ClosetHidingSystem : MonoBehaviour
 
     public void GoOutsideCloset()
     {
+
+        player.position = exitPoint.position;
+        player.rotation = exitPoint.rotation;
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
         VcamCloset.SetActive(false);
         VcamPlayer.SetActive(true);
+        InsideCloset = false;
     }
 }
