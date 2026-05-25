be8b2dd5227fe204077579233c4b6f15236389f0
JasonLomotos <jasontlomotos@iskolarngbayan.pup.edu.ph>
2026-05-26 00:43:27 +0800

    Level1 and Boss level design update

---
+++ Assets/Scripts/Other/LightFlicker.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light light;
+
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    private void Start()
+    {
+       light = GetComponent<Light>();
+
+       InvokeRepeating("Flicker", 0f, flickerSpeed);
+
+    }
+
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
+    }
+
+
+
+}
